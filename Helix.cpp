edb30ff38d42ca1d756721a391c441ba31aacd25
Alex Wayne <alex@beautifulpixel.com>
2014-08-25 20:22:33 -0700

    Helix is now WAAY cooler.

--- Helix.cpp
+++ Helix.cpp
@@ -1,51 +1,55 @@
 #include "Helix.h"
 
+uint8_t usin8(uint8_t input) {
+  int16_t result16 = sin16(input << 8) + 32767;
+  return result16 >> 8;
+}
+
 void Helix::start() {
   name = "Helix";
-  numChannels = 4;
+  numChannels = 3;
 
-  channels[0].name = "Speed 1";
+  channels[0].name = "Quantity";
   channels[0].type = ProgPropVarRange;
-  channels[0].value = 127; // 0-255 mapped from min to max
+  channels[0].value = 0xFF/5; // 0-255 mapped from min to max
 
-  channels[1].name = "Hue 1";
+  channels[1].name = "Move Speed";
   channels[1].type = ProgPropVarRange;
-  channels[1].value = 0; // 0-255 mapped from min to max
+  channels[1].value = 127; // 0-255 mapped from min to max
 
-  channels[2].name = "Speed 2";
+  channels[2].name = "Hue Speed";
   channels[2].type = ProgPropVarRange;
-  channels[2].value = 127; // 0-255 mapped from min to max
-
-  channels[3].name = "Hue 2";
-  channels[3].type = ProgPropVarRange;
-  channels[3].value = 0; // 0-255 mapped from min to max
+  channels[2].value = 0; // 0-255 mapped from min to max
 };
 
 void Helix::update() {
+  elapsed += channels[1].value << 2;
+  hue     += channels[2].value << 2;
+
   for (uint8_t i = 0; i < LED_COUNT; i++) {
     leds[i] = CRGB::Black;
   }
 
-  draw(channels[0].value, channels[1].value, false);
-  draw(channels[2].value, channels[3].value, true);
+  uint8_t qty = lerp8by8(1, 8, channels[0].value);
+  uint8_t offset = 0xFF / qty;
+  for (uint8_t i = 0; i < qty; i++) {
+    draw(offset * i);
+  }
 
   FastLED.show();
 };
 
 
-void Helix::draw(uint8_t speed, uint8_t hue, bool mirror) {
+void Helix::draw(uint8_t offset) {
   const uint8_t radius = 64;
   const uint8_t innerRadius = 50;
 
-  // Speed channel represents how many bits we shift to the left
-  uint8_t shiftAmount = lerp8by8(1, 8, speed);
-  uint8_t pos8 = (sin16(millis() << shiftAmount) + 0xFFFF/2) >> 8;
-  if (mirror) pos8 = 0xFF - pos8;
+  uint8_t pos8 = usin8(offset + (elapsed >> 8));
 
-  CHSV color = CHSV(hue, 0xFF, 0);
+  CHSV color = CHSV((hue >> 8) + offset, 0xFF, 0);
 
   for (uint8_t i = 0; i < LED_COUNT; i++) {
-    int i8 = map(i, 0, LED_COUNT-1, -radius+1, 0xFF+radius-1);
+    int i8 = map(i, 0, LED_COUNT-1, -innerRadius, 0xFF+innerRadius);
     int brightness      = radius - abs(pos8 - i8);
     int innerBrightness = innerRadius - abs(pos8 - i8);
 
--- Helix.h
+++ Helix.h
@@ -5,9 +5,12 @@
 
 class Helix : public Program {
   public:
+    uint16_t elapsed;
+    uint16_t hue;
+
     void start();
     void update();
-    void draw(uint8_t speed, uint8_t hue, bool mirror);
+    void draw(uint8_t offset);
 };
 
 #endif
